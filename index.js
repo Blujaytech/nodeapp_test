90b572c52cad9af9bf5f870726e63f94636cabc7
Blujaytech <blujaytechnlogies@gmail.com>
2026-05-16 16:04:16 +0530

    Update response message for '/ready' endpoint

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Blujay Trainer HariKrishna Cherukuu" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, Get Knowledge from Blujaytech!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
