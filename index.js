c8df11135e20c85c938021af521e43701ff610d2
Blujaytech <blujaytechnlogies@gmail.com>
2026-05-19 13:21:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "BlujayTech is No.1 AWSDevops Traning Center In ameerpet" }');
+    res.send('{ "response": "BlujayTech is No.1 AWSDevops Traning Center In Hyderabad, https://www.youtube.com/@blujaytech" }');
 });
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "Blujay Trainer HariKrishna Cheruku" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, Get Knowledge from Blujaytech1!" }');
+    res.send('{ "response": " Great!, https://www.youtube.com/@blujaytech!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
